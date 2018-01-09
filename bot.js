344886a07ff85ddbf4feb5963ddcb621000fe54b
elann <elann.vit@gmail.com>
2018-01-09 22:03:49 +0100

    hour UTC

--- bot.js
+++ bot.js
@@ -29,6 +29,8 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+var list_ww = [];
+
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -40,24 +42,33 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // !ww heure_deb nbr_minutes
             case 'ww':
                 //Enought arguments
-                if (args.length == 3) {
+                if (args.length >= 3) {
                     var deb_hour = args[1].split(/[a-z]|:/);
                 }
                 else {
                     bot.sendMessage({
                         to: channelID,
-                        message: 'Veuillez indiquer votre Word War au format ```!ww heureDeDépart NombreDeMinutes```'
+                        message: 'Veuillez indiquer votre Word War au format ```!ww heureDeDépart NombreDeMinutes [NomDeLaWW] (les arguments entre crochets sont optionnels)```'
                     });
                     break
                 }
              
-                //Get hour and minutes
+                //Get hour and minutes and duration of the WW
                 var hour = parseInt(deb_hour[0]);
                 if (deb_hour[1] !== "") {
                     var minutes = parseInt(deb_hour[1]);
                 }
                 else {
                     var minutes = 00;
+                }
+                var nbr_minutes_ww = parseInt(args[2]);
+                //Get name
+                var name_ww = "";
+                if (args.length == 4) {
+                    name_ww = args[3];
+                }
+                else {
+                    name_ww = args[1]+args[2];
                 }
                 
                 //Hour and minutes are between 0-24 and 0-59
@@ -66,7 +77,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     var heure_deb_ww = new Date();
                     heure_deb_ww.setHours(hour, minutes, 0);
                     var heure_fin_ww = new Date(heure_deb_ww);
-                    heure_fin_ww.setMinutes(heure_deb_ww.getMinutes() + parseInt(args[2]));
+                    heure_fin_ww.setMinutes(heure_deb_ww.getMinutes() + nbr_minutes_ww);
                     
                     var deb_hour = heure_deb_ww.getHours();
                     var fin_hour = heure_fin_ww.getHours();
@@ -85,13 +96,16 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         heure_fin_ww.setDate(heure_fin_ww.getDate()+1);
                     }
                     
+                    //Add WW to list
+                    list_ww.push([name_ww,heure_deb_ww,heure_fin_ww]);
+                    
                     //Message acceptation of the WW
                     bot.sendMessage({
                         to: channelID,
-                        message: 'Ok, WW à ' + deb_hour + 'h' + deb_min + ' pour ' + args[2] + ' minutes ! Fin à ' + fin_hour + 'h' + fin_min + ' !'
+                        message: 'Ok, WW ' + name_ww + 'à ' + deb_hour + 'h' + deb_min + ' pour ' + args[2] + ' minutes ! Fin à ' + fin_hour + 'h' + fin_min + ' !'
                     });
                     
-                    console.log("heure de début " + heure_deb_ww);
+                    console.log("Liste des WW " + list_ww);
                     
                     //Schedule the beginning of the WW
                     schedule.scheduleJob('WW', heure_deb_ww, function(params)
